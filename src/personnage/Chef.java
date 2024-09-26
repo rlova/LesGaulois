e59ca9cf4d68da92ce0f2b44bd86b9249bb5299e
rlova <lovaremano@gmail.com>
2024-09-26 22:38:02 +0200

    ajout class Chef

--- src/personnage/Chef.java
+++ src/personnage/Chef.java
@@ -1,38 +1,30 @@
 package personnage;
 
-public class Gaulois {
+public class Chef {
 	private String nom;
 	private int force;
-	private int effetPotion = 1;
-
-	public Gaulois(String nom, int force) {
+	private Village village;
+	
+	public Chef(String nom, int force, Village village) {
 		this.nom = nom;
 		this.force = force;
+		this.village = village;
 	}
 
 	public String getNom() {
 		return nom;
 	}
-
+	
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "<< " + texte + ">>");
 	}
 	
 	private String prendreParole() {
-		return "Le gaulois " + nom + " : ";
+		return "Le chef " + nom + " du village " + village.getNom() + " : ";
 	}
 	
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
 		romain.recevoirCoup(force / 3);
-	}
-
-	@Override
-	public String toString() {
-		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
-	}
-
-	public static void main(String[] args) {
-
 	}
 }
