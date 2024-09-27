d26a85e20055d0737fe72d2348aa04a7e8149731
rlova <lovaremano@gmail.com>
2024-09-27 11:38:35 +0200

    ajout prer

--- src/histoire/Druide.java
+++ src/histoire/Druide.java
@@ -1,4 +1,27 @@
 package histoire;
 
+import java.util.Random;
+
 public class Druide {
 	private int forcePotion = 1;
+	private int effetPotionMax;
+	
+	public Druide(int effetPotionMax) {
+		this.effetPotionMax = effetPotionMax;
+	}
+
+	public void preparerPotion () {
+		Random random = new Random();
+		forcePotion = random.nextInt(effetPotionMax);
+		if (forcePotion > 7) {
+			System.out.println(" J'ai préparé une super potion de force " + forcePotion);
+		} else {
+			System.out.println(" Je n'ai pas trouvé tous les ingrédients, ma potion est seulement de force " + forcePotion);
+		}
+	}
+	
+	public static void main(String[] args) {
+		Druide panoramix = new Druide(10);
+		panoramix.preparerPotion();
+	}
+}
