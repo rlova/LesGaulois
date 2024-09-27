e01f5d6589254ef6e7e122bc8e101c267a1fa006
rlova <lovaremano@gmail.com>
2024-09-27 12:26:40 +0200

    modifier méthode frapper

--- src/personnage/Gaulois.java
+++ src/personnage/Gaulois.java
@@ -29,7 +29,7 @@ public class Gaulois {
 	
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		romain.recevoirCoup(force / 3);
+		romain.recevoirCoup((force / 3)*effetPotion);
 	}
 
 	@Override
