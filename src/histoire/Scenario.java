c8eee144d12eb0f2cbca1dd2e8e925abb4f71de8
rlova <lovaremano@gmail.com>
2024-09-27 14:10:12 +0200

    modification class Scenario,class Gaulois

--- src/histoire/Scenario.java
+++ src/histoire/Scenario.java
@@ -4,22 +4,22 @@ import personnage.Gaulois;
 import personnage.Romain;
 
 public class Scenario {
-
 	public static void main(String[] args) {
 		Druide panoramix = new Druide(5,10);
+		System.out.println("Le druide Panoramix : << Bonjour, je suis le druide Panoramix et ma potion peut aller d'une force " + panoramix.getEffetPotionMin() + " à " + panoramix.getEffetPotionMax() + " >>");
+		System.out.println("Le druide Panoramix : << Je vais aller préparer une petite potion... >>");
+		System.out.print("Le druide Panoramix : << ");
+		panoramix.preparerPotion();
+		System.out.print("Le druide Panoramix : <<");
 		Gaulois asterix = new Gaulois("Asterix",8);
 		Romain minus = new Romain("Minus",6);
 		Gaulois obelix = new Gaulois("Obelix",25);
-		System.out.println(" Le druide Panoramix : << Bonjour, je suis le druide Panoramix et ma potion peut aller d'une force " + panoramix.geteffetPotionMin() + " à " + + " >>");
-		System.out.println(" Le druide Panoramix : << Je vaus aller préparer une petite potion... >>");
-		System.out.print(" Le druide Panoramix : ");
-		panoramix.preparerPotion();
-		System.out.println(" Le druide Panoramix : << ");
-		asterix.parler("Bonjour à tous");
-		minus.parler("UN GAU... UN GAUGAU...");
-		asterix.frapper(minus);
-		asterix.frapper(minus);
-		asterix.frapper(minus);
+		panoramix.booster(obelix);
+		System.out.print("Le gaulois Asterix : <<");
+		asterix.boirePotion(6);		
+		asterix.parler("Bonjour");		
+		minus.parler("UN GAU... UN GAUGAU...");		
+		asterix.frapper(minus);	
+		minus.parler("J'abandonne...");	
 	}
-
 }
