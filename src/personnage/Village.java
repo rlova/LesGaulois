4754c5723b898d105a1b7893298a9649519ba3dc
rlova <lovaremano@gmail.com>
2024-09-28 20:44:52 +0200

    modification class Village

--- src/personnage/Village.java
+++ src/personnage/Village.java
@@ -2,12 +2,35 @@ package personnage;
 
 public class Village {
 	private String nom;
+//	private Chef chef;
+	private static Gaulois[] villageois;
+	private int nbVillageois;
 	
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMaximum) {
 		this.nom = nom;
+		this.nbVillageois = 0;
+		this.villageois = new Gaulois[nbVillageoisMaximum];
 	}
+
+	public void ajouterHabitant(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois++;
+	}
+	
+	public static Gaulois trouverHabitant(int numeroVillageois) {
+		return villageois[numeroVillageois];
+	}
+	
+//	public void setChef(Chef chef) {
+//		this.chef = chef;
+//	}
 
 	public String getNom() {
 		return nom;
+	}
+	
+	public static void main(String[] args) {
+		Village village = new Village("Village des Irréductibles",30);
+		Gaulois gaulois = village.trouverHabitant(30);
 	}
 }
