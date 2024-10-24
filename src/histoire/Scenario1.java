e137497ca4c72d2c13875e0228a39a331016b23a
rlova <lovaremano@gmail.com>
2024-10-24 21:05:00 +0200

    change classe Romain, Scenario1

--- src/histoire/Scenario1.java
+++ src/histoire/Scenario1.java
@@ -12,13 +12,13 @@ public class Scenario1 {
 		Gaulois asterix = new Gaulois("Asterix",8);
 		Romain minus = new Romain("Minus",6);
 		Gaulois obelix = new Gaulois("Obelix",25);
-		panoramix.prendreParole();
+		System.out.print("Le druide " + panoramix.getNom() + " : ");
 		panoramix.booster(obelix);
 		obelix.parler("Par Bélénos, ce n'est pas juste !");
-		System.out.print("Le gaulois Asterix : <<");
+		System.out.print("Le gaulois " + asterix.getNom() + " : ");	
 		asterix.boirePotion(6);		
-//		asterix.parler("Bonjour");		
-//		minus.parler("UN GAU... UN GAUGAU...");
+		asterix.parler("Bonjour");		
+		minus.parler("UN GAU... UN GAUGAU...");
 		asterix.frapper(minus);
 //		minus.parler("J'abandonne...");	
 	}
